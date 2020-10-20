f079933d6653ea9a169186921d8b7eac75fce562
Katie <adlerk8@gmail.com>
2020-10-20 14:31:29 -0700

    evenAndOdds Done

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -34,7 +34,7 @@ export default class EvenAndOdds extends Component {
             <div className="puzzleBox evenAndOddPB">
                 <h4>Evens and Odds</h4>
                 <input className="inputLine" onChange={e => this.handleChange(e.target.value)}></input>
-                <button className="confirmationButton" onClick={ () => {this.assignEvenAndOdds(this.state.userInput)}}></button>
+                <button className="confirmationButton" onClick={ () => {this.assignEvenAndOdds(this.state.userInput)}}>Split</button>
                 <span className="resultsBox"> Evens: {JSON.stringify(this.state.evenArray) }</span>
                 <span className="resultsBox"> Odds: {JSON.stringify(this.state.oddArray) }</span>
             </div>
