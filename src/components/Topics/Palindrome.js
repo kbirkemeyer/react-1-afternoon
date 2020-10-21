691df09300c1e680ef53af00972e4abeff475046
Katie <adlerk8@gmail.com>
2020-10-20 17:23:37 -0700

    FilterString done, palindrome in progress

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Palindrome extends Component {
+    constructor() {
+        super();
+        this.state = {
+            userInput: "",
+            palindrome: ""
+        };
+    }
+    
+handleChange(val) {
+    this.setState({userInput: val});
+}
+
+palindromer(userInput) {
+
+}
+
     render() {
         return(
-            <p>Palindrome Component</p>
+            <div className="puzzleBox palindromePB">
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}/>
+                <button className="confirmationButton" onClick={() => this.palindromer(this.state.userInput)}></button>
+                <span className="resultsBox"> Palindrome: {this.state.palindrome}</span>
+            </div>
         )
     }
 }
